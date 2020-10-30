85417d7f9adf7a0b39cf86c485cdb3e8a969ae11
Natasha <nmaniar.mit.edu>
2020-10-30 11:38:59 -0700

    added pose matching

--- Tests/Koplow/socketANDposenet/index.js
+++ Tests/Koplow/socketANDposenet/index.js
@@ -14,6 +14,29 @@ for(let i = 1; i <= numRooms; i++){
   rooms.push(new room("Room "+i))
 }
 
+function weightedDistanceMatching(poseVector1, poseVector2) {
+  let vector1PoseXY = poseVector1.slice(0, 34);
+  let vector1Confidences = poseVector1.slice(34, 51);
+  let vector1ConfidenceSum = poseVector1.slice(51, 52);
+
+  let vector2PoseXY = poseVector2.slice(0, 34);
+
+      // First summation
+  let summation1 = 1 / vector1ConfidenceSum;
+
+      // Second summation
+  let summation2 = 0;
+  for (let i = 0; i < vector1PoseXY.length; i++) {
+    let tempConf = Math.floor(i / 2);
+    let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
+    summation2 = summation2 + tempSum;
+  }
+
+  return summation1 * summation2; 
+}
+
+
+
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
@@ -49,6 +72,30 @@ io.on('connection', (socket) => {
     socket.on('senduserpos', (senduserpos) => {
       let idx = playerRoom.players.indexOf(socket.id);
       playerRoom.player_positions[idx]=senduserpos
+      var playerScores = []
+      function createArray(p){
+        let posevector1 = []
+        var summ = 0;
+        for (let i=0; i<playerRoom.player_positions[p].length; i++){
+          posevector1.push(playerRoom.player_positions[p][i].position.x);
+          posevector1.push(playerRoom.player_positions[p][i].position.y);
+        }
+        for (let i=0; i<playerRoom.player_positions[p].length; i++){
+          posevector1.push(playerRoom.player_positions[p][i].score);
+          summ+=playerRoom.player_positions[p][i].score;
+        }
+        posevector1.push(summ);
+        return posevector1;
+      }
+      var compare = [50]*52;// CHANGE this to library of poses
+      for (let i=0; i<playerRoom.player_positions.length; i++){
+        let playerPose = createArray(i); 
+        let score = weightedDistanceMatching(playerPose,compare); 
+        playerScores.push(score);
+      }
+      var winner = playerScores.indexOf(Math.min.apply(Math, playerScores));
+      console.log(winner); 
+
     });
 
     socket.on('getroomname', () => {
