98b05b2c391e6754e6573f438a20d9e071c4d43d
Natasha <nmaniar.mit.edu>
2020-11-14 09:36:57 -0800

    fix changes

--- Tests/Koplow/socketANDposenet/public/client.js
+++ Tests/Koplow/socketANDposenet/public/client.js
@@ -1,13 +1,11 @@
-
 function initClient(socket){
 
-    $('form').submit(function(e) {
-      e.preventDefault(); // prevents page reloading
-      return false;
-    });
-    socket.on('roomdata', function(r){
-      ROOM=r;
-    });
+  $('form').submit(function(e) {
+    e.preventDefault(); // prevents page reloading
+    return false;
+  });
+  socket.on('roomdata', function(r){
+    ROOM=r[0];
+    HOLE=r[1]
+  });
 }
-  
-  
